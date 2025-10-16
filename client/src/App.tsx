c905807d625ba420cf405f788c03a8611ca3ba8e
Aditya <adityasainics10@gmail.com>
2025-10-16 14:15:37 +0530

    initialize front-end with react

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-800'>
+        hey
+      </div>
+      <Button>
+        hey bhai
+      </Button>
+    </>
+  )
+}
+
+export default App
